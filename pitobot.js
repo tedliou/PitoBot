b3f6c153585f8c015695bdf863b809a578b2e86a
SHA256 <s1010193@gmail.com>
2018-07-03 00:25:44 +0800

    Update code
    新增隨機播放功能、help指令
    刪除clear,、resume
    play 取代原先的 resume
    效能優化

--- pitobot.js
+++ pitobot.js
@@ -1,9 +1,8 @@
 const Discord = require('discord.js');
-//const Google = require('googleapis');
-const ytdl = require('ytdl-core');
-const fs = require('fs');
 const request = require('request');
 const cheerio = require('cheerio');
+const ytdl = require('ytdl-core');
+const fs = require('fs');
 
 // Import config.json Setting
 console.log('Loading config.json');
@@ -21,6 +20,7 @@ client.on('ready', () => {
   if (!fs.existsSync(filePath)) {
     fs.mkdirSync(filePath);
   }
+  console.log('Done！');
 });
 
 // When change server region
@@ -38,173 +38,78 @@ client.on('guildUpdate', (oldGuild, newGuild) => {
   }
 });
 
-function validURL(url) {
-  if (url.startsWith('https://www.youtube.com/playlist?list=')) {
-    return 1;
-  } else if (ytdl.validateURL(url)) {
-    return 2;
-  } else {
-    return 0;
-  }
-}
-
-function addPlaylist(url, message) {
-
-
-  request(url, (err, res, body) => {
-    var $ = cheerio.load(body);
-    var tr = $('#pl-load-more-destination tr');
-    var filePath = './data/' + message.guild.id + '.json';
-
-    const embed = new Discord.RichEmbed()
-    .setTitle('讀取 ' + tr.length + ' 首歌曲 ...')
-    .setAuthor('正在擷取播放清單')
-    .setFooter('Pitohui Music Bot')
-    .setTimestamp()
-    .setColor(0x00AE86);
-    message.channel.send(embed).then(msg => {
-      if (config.Setting.MessageDelete.enable == true) {
-        msg.delete(config.Setting.MessageDelete.delay * 1000);
-      }
-
-
-      fs.readFile(filePath, 'utf8', (err, data) => {
-        try {
-          var json = JSON.parse(data);
-
-        } catch (e) {
-          console.log(e);
-          var json = new Array();
-        }
-        for (let i = 1; i < tr.length; i++) {
-          let videoID = tr.eq(i).attr('data-video-id');
-          let videoTitle = tr.eq(i).attr('data-title');
-          json.push(new Array(videoID, videoTitle));
-        }
-
-        fs.writeFile(filePath, JSON.stringify(json), (err) => {
-          if (err) throw err;
-
-          const embed = new Discord.RichEmbed()
-          .setTitle('讀取完成')
-          .setAuthor('已加到佇列')
-          .setFooter('Pitohui Music Bot')
-          .setTimestamp()
-          .setColor(0x00AE86);
-          msg.edit(embed);
-          /*message.channel.send(embed).then(msg => {
-            if (config.Setting.MessageDelete.enable == true) {
-              msg.delete(config.Setting.MessageDelete.delay * 1000);
-            }
-          });*/
-
-          if(!message.guild.voiceConnection.dispatcher) {
-            if (json.length > tr.length) {
-              message.reply('佇列中尚有待播放的音樂，請輸入 ' + config.Setting.CommandPrefix + 'play 進行播放');
-            } else {
-              playSound(message);
-            }
-          }
-        });
-      });
-
-
-    });
-
-
-  });
-}
-
 // When user input message
 client.on('message', message => {
   const command = message.content.toLowerCase();
-
   // Play url Command
-  if (command.startsWith(config.Setting.CommandPrefix + 'play ')) {
+  if (command.startsWith(config.Setting.Prefix + config.Command.Play + ' ')) {
     if (message.member.voiceChannel) {
       message.member.voiceChannel.join()
         .then(connection => {
           let url = message.content.split(' ').slice(1).toString();
 
-          switch (validURL(url)) {
-            case 0:
-              message.reply('未知的來源');
-              break;
-            case 1:
-              addPlaylist(url, message);
-              break;
-            case 2:
-              addQueue(url, message);
-              break;
+          if (url.startsWith('https://www.youtube.com/playlist?list=')) {
+            addPlaylist(url, message);
+          } else if (ytdl.validateURL(url)) {
+            addSound(url, message);
+          } else {
+            message.reply('未知的來源');
           }
+
         });
     } else {
-      message.reply('請先加入語音頻道再進行操作');
+      message.reply(config.Message.PleaseJoinChannel);
     }
-    autoDeleteMessage(message);
   }
 
-  else if (command === config.Setting.CommandPrefix + 'play') {
+  // Play & Resume
+  else if (command === config.Setting.Prefix + config.Command.Play) {
     if (message.member.voiceChannel) {
       message.member.voiceChannel.join();
-      if (!message.member.voiceChannel.dispatcher) {
+      if (!message.guild.voiceConnection.dispatcher) {
         playSound(message);
+      } else if (message.guild.voiceConnection.dispatcher.paused) {
+        message.guild.voiceConnection.dispatcher.resume();
       }
     } else {
-      message.reply('請先加入語音頻道再進行操作');
+      message.reply(config.Message.PleaseJoinChannel);
     }
-    autoDeleteMessage(message);
   }
 
   // Stop Command
-  else if (command === config.Setting.CommandPrefix + 'stop') {
+  else if (command === config.Setting.Prefix + config.Command.Stop) {
     if (message.member.voiceChannel) {
-      if (message.guild.voiceConnection) {
-        message.guild.voiceConnection.disconnect();
+      if (message.guild.voiceConnection && message.guild.voiceConnection.dispatcher) {
+        message.guild.voiceConnection.dispatcher.end('stop');
+        message.reply('停止音樂');
       }
-      message.reply('停止音樂');
     } else {
-      message.reply('請先加入語音頻道再進行操作');
+      message.reply(config.Message.PleaseJoinChannel);
     }
-
-    autoDeleteMessage(message);
   }
 
   // Skip Command
-  else if (command === config.Setting.CommandPrefix + 'skip') {
+  else if (command === config.Setting.Prefix + config.Command.Skip) {
     if (message.guild.voiceConnection && message.guild.voiceConnection.dispatcher) {
       message.guild.voiceConnection.dispatcher.end();
-      message.reply('跳過音樂');
+      message.reply('切歌');
     } else {
-      message.reply('請先加入語音頻道再進行操作');
+      message.reply(config.Message.PleaseJoinChannel);
     }
-    autoDeleteMessage(message);
   }
 
   // Pause Command
-  else if (command === config.Setting.CommandPrefix + 'pause') {
+  else if (command === config.Setting.Prefix + config.Command.Pause) {
     if (message.guild.voiceConnection && message.guild.voiceConnection.dispatcher) {
       message.guild.voiceConnection.dispatcher.pause();
       message.reply('暫停音樂');
     } else {
-      message.reply('請先加入語音頻道再進行操作');
+      message.reply(config.Message.PleaseJoinChannel);
     }
-    autoDeleteMessage(message);
-  }
-
-  // Resume Command
-  else if (command === config.Setting.CommandPrefix + 'resume') {
-    if (message.guild.voiceConnection && message.guild.voiceConnection.dispatcher) {
-      message.guild.voiceConnection.dispatcher.resume();
-      message.reply('恢復音樂');
-    } else {
-      message.reply('請先加入語音頻道再進行操作');
-    }
-    autoDeleteMessage(message);
   }
 
   // Now Playing Command
-  else if (command === config.Setting.CommandPrefix + 'np') {
+  else if (command === config.Setting.Prefix + config.Command.Nowplaying) {
     var filePath = './data/' + message.guild.id + '.json';
     fs.readFile(filePath, 'utf8', (err, data) => {
       try {
@@ -214,24 +119,19 @@ client.on('message', message => {
           .setTitle(json[0][1])
           .setURL('https://www.youtube.com/watch?v=' + json[0][0])
           .setAuthor('正在播放')
-          .setFooter('Pitohui Music Bot')
+          .setFooter(config.Setting.BotName)
           .setTimestamp()
           .setColor(0x00AE86);
-          message.channel.send(embed).then(msg => {
-            if (config.Setting.MessageDelete.enable == true) {
-              msg.delete(config.Setting.MessageDelete.delay * 1000);
-            }
-          });
+          message.channel.send(embed);
         }
       } catch (e) {
         console.log(e);
       }
     });
-    autoDeleteMessage(message);
   }
 
   // Queue Command
-  else if (command === config.Setting.CommandPrefix + 'queue') {
+  else if (command === config.Setting.Prefix + config.Command.Queue) {
     var filePath = './data/' + message.guild.id + '.json';
     fs.readFile(filePath, 'utf8', (err, data) => {
       try {
@@ -239,60 +139,105 @@ client.on('message', message => {
         if (json.length > 0) {
           const embed = new Discord.RichEmbed()
           .setAuthor('播放佇列')
-          .setFooter('Pitohui Music Bot')
+          .setFooter(config.Setting.BotName)
           .setTimestamp()
           .setColor(0x00AE86);
           for (i=0;i<json.length&&i<25;i++) {
             embed.addField(json[i][1], 'https://www.youtube.com/watch?v=' + json[i][0], true);
           }
 
-          message.channel.send(embed).then(msg => {
-            if (config.Setting.MessageDelete.enable == true) {
-              autoDeleteMessage(msg);
-            }
-          });
+          message.channel.send(embed);
         } else {
           const embed = new Discord.RichEmbed()
           .setAuthor('播放佇列')
           .setTitle('沒有歌曲')
-          .setFooter('Pitohui Music Bot')
+          .setFooter(config.Setting.BotName)
           .setTimestamp()
           .setColor(0x00AE86);
-          message.channel.send(embed).then(msg => {
-            if (config.Setting.MessageDelete.enable == true) {
-              autoDeleteMessage(msg);
-            }
-          });
+          message.channel.send(embed);
         }
       } catch (e) {
         console.log(e);
       }
     });
-
-    autoDeleteMessage(message);
   }
 
-  // Clear Command
-  else if (command === config.Setting.CommandPrefix + 'clear') {
-    message.reply('清除音樂佇列');
-    var filePath = './data/' + message.guild.id + '.json';
-    var json = new Array();
-    fs.writeFile(filePath, JSON.stringify(json), (err) => {
-      if (err) throw err;
-    });
+  // Help Command
+  else if (command === config.Setting.Prefix + config.Command.Help) {
+    const embed = new Discord.RichEmbed()
+    .setAuthor('指令說明')
+    .setFooter(config.Setting.BotName)
+    .setTimestamp()
+    .setColor(0x00AE86);
+    var cmds = '';
+    for(var cmd in config.Command) {
+      cmds += config.Setting.Prefix + config.Command[cmd] + '\n';
+    }
+    embed.setDescription(cmds);
+    message.channel.send(embed);
   }
 });
 
-
-
-
-function autoDeleteMessage(message) {
-  if (config.Setting.MessageDelete.enable == true) {
-    message.delete(config.Setting.MessageDelete.delay * 1000);
+function arrayShuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
 }
 
-function addQueue(url, message) {
+function addPlaylist(url, message) {
+
+  var embed = new Discord.RichEmbed()
+  .setTitle('讀取中...')
+  .setAuthor('正在讀取')
+  .setFooter(config.Setting.BotName)
+  .setTimestamp()
+  .setColor(0x00AE86);
+
+  message.channel.send(embed).then(msg => {
+    request(url, (error, response, body) => {
+      var filePath = './data/' + message.guild.id + '.json';
+      fs.readFile(filePath, 'utf8', (err, data) => {
+        var json = JSON.parse(data);
+        var tmp = new Array();
+        var $ = cheerio.load(body);
+        var tr = $('#pl-load-more-destination tr').each((index, element) => {
+          let videoID = $(element).attr('data-video-id');
+          let videoTitle = $(element).attr('data-title');
+          tmp.push(new Array(videoID, videoTitle));
+        });
+
+        if (config.Setting.ShufflePlaylist) {
+          arrayShuffle(tmp);
+        }
+
+        json = json.concat(tmp);
+
+        fs.writeFile(filePath, JSON.stringify(json), (err) => {
+          if (err) throw err;
+          const embed = new Discord.RichEmbed()
+          .setTitle(':white_check_mark: 讀取完成')
+          .setAuthor('新增 ' + tr.length + ' 首歌曲至播放清單')
+          .setFooter(config.Setting.BotName)
+          .setTimestamp()
+          .setColor(0x00AE86);
+          msg.edit(embed);
+
+          if(!message.guild.voiceConnection.dispatcher) {
+            if (json.length > tr.length) {
+              message.reply('佇列中尚有待播放的音樂，請輸入 ' + config.Setting.Prefix + 'play 進行播放');
+            } else {
+              playSound(message);
+            }
+          }
+
+        });
+      });
+    });
+  });
+}
+
+function addSound(url, message) {
   var filePath = './data/' + message.guild.id + '.json';
   var videoID = ytdl.getURLVideoID(url);
   ytdl.getInfo(videoID, (err, info) => {
@@ -302,35 +247,23 @@ function addQueue(url, message) {
         var json = new Array(new Array(videoID, videoTitle));
       }
       else {
-        try {
-          var json = JSON.parse(data);
-        } catch (e) {
-          var json = new Array();
-        }
+        var json = JSON.parse(data);
         json.push(new Array(videoID, videoTitle));
 
         const embed = new Discord.RichEmbed()
-        .setTitle(info.title)
+        .setTitle(':white_check_mark: ' + info.title)
         .setURL(info.video_url)
         .setAuthor('已加到佇列')
-        .setFooter('Pitohui Music Bot')
+        .setFooter(config.Setting.BotName)
         .setTimestamp()
         .setColor(0x00AE86);
-        message.channel.send(embed).then(msg => {
-          if (config.Setting.MessageDelete.enable == true) {
-            msg.delete(config.Setting.MessageDelete.delay * 1000);
-          }
-        });
-
+        message.channel.send(embed);
       }
+
       fs.writeFile(filePath, JSON.stringify(json), (err) => {
         if (err) throw err;
         if(!message.guild.voiceConnection.dispatcher) {
-          if (json.length > 1) {
-            message.reply('佇列中尚有待播放的音樂，請輸入 ' + config.Setting.CommandPrefix + 'play 進行播放');
-          } else {
-            playSound(message);
-          }
+          playSound(message);
         }
       });
     });
@@ -339,59 +272,50 @@ function addQueue(url, message) {
 
 function playSound(message) {
   var filePath = './data/' + message.guild.id + '.json';
+
   fs.readFile(filePath, 'utf8', (err, data) => {
-    try {
-      var json = JSON.parse(data);
+    var json = JSON.parse(data);
 
-    } catch (e) {
-      console.log(e);
-      var json = new Array();
-    }
+    if (json.length > 0) {
+      const embed = new Discord.RichEmbed()
+      .setTitle(json[0][1])
+      .setURL('https://www.youtube.com/watch?v=' + json[0][0])
+      .setAuthor('正在播放')
+      .setFooter(config.Setting.BotName)
+      .setTimestamp()
+      .setColor(0x00AE86);
+      message.channel.send(embed);
 
-    var stream = ytdl('https://www.youtube.com/watch?v=' + json[0][0], {filter: 'audio'});
-    const embed = new Discord.RichEmbed()
-    .setTitle(json[0][1])
-    .setURL('https://www.youtube.com/watch?v=' + json[0][0])
-    .setAuthor('正在播放')
-    .setFooter('Pitohui Music Bot')
-    .setTimestamp()
-    .setColor(0x00AE86);
-    message.channel.send(embed).then(msg => {
-      if (config.Setting.MessageDelete.enable == true) {
-        msg.delete(config.Setting.MessageDelete.delay * 1000);
-      }
-    });
-    try {
-      message.guild.voiceConnection.playStream(stream, {
+      var stream = ytdl('https://www.youtube.com/watch?v=' + json[0][0], {filter: 'audio'});
+      var dispatcher = message.guild.voiceConnection.playStream(stream, {
         passes: 2,
         bitrate: 'auto'
       });
-      message.guild.voiceConnection.dispatcher.once('end', function() {
-        fs.readFile(filePath, 'utf8', (err, data) => {
-          try {
-            var json = JSON.parse(data);
 
-          } catch (e) {
-            console.log(e);
-            var json = new Array();
-          }
-          json.shift();
+      dispatcher.once('end', (reason) => {
+        if (reason === 'stop') {
+          message.guild.voiceConnection.disconnect();
+          var json = new Array();
           fs.writeFile(filePath, JSON.stringify(json), (err) => {
             if (err) throw err;
-            if (json.length > 0 && message.guild.voiceConnection) {
-              setImmediate(() => {
-                playSound(message);
-              });
-            } else {
-              if (message.guild.voiceConnection) {
+          });
+        } else {
+          fs.readFile(filePath, 'utf8', (err, data) => {
+            var json = JSON.parse(data);
+            json.shift();
+            fs.writeFile(filePath, JSON.stringify(json), (err) => {
+              if (err) throw err;
+              if (json.length > 0) {
+                setImmediate(() => {
+                  playSound(message);
+                });
+              } else {
                 message.guild.voiceConnection.disconnect();
               }
-            }
+            });
           });
-        });
+        }
       });
-    } catch (e) {
-      //console.log(e);
     }
   });
 }
